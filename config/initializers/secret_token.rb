cd22c70bb2e37ed5df06eaf3ac89e183c4f1cdc6
ml242 <mattlucas@gmail.com>
2014-07-29 21:32:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClimberCore::Application.config.secret_key_base = '42def7fe996f79f91a214c0e4a8cef6ae848404e37d106e148524a48f34b54cf784db2014a85a7f88a256bf48e6fccb179d4ffce083be9d97ad5c9d9e853135b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClimberCore::Application.config.session_store :cookie_store, key: '_climber_core_session'
