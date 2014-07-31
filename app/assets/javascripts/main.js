7f187bc73bb71ca33c9989c11e4c1ff9dc10dbd7
ml242 <mattlucas@gmail.com>
2014-07-31 12:01:15 -0400

    styles coming along

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,42 @@
 $(function(){
 
-  $('section').each(function(i,el) {
+// section styles
+
+  $('section').each(function(i, el) {
     // console.log(el);
     el.style.height = $(window).height() + 'px';
   });
 
+  var firstSectionHeight = $(window).height() - 128 + 'px';
+
+  $('.main-intro').css({height: firstSectionHeight}); 
+
   $('.large-image-1').css({backgroundImage: 'url(http://media.giphy.com/media/QnWWQ1tVagHra/giphy.gif)'});
+
+
+
+// slider functionality
+
+  var slider = $('.testimonials-1');
+
+  $('.right-button').click(function(){
+    if ( slider.hasClass('testimonials-4') ) { 
+      slider = $('.testimonials-1');
+      slider.show().siblings().hide();
+    } else {
+      slider = slider.next();
+      slider.show().siblings().hide();
+      }
+    });
+
+  $('.left-button').click(function(){
+    if ( slider.hasClass('testimonials-1') ) { 
+      slider = $('.testimonials-4');
+      slider.show().siblings().hide();
+    } else {
+      slider = slider.prev();
+      slider.show().siblings().hide();
+      }
+    });
 
 });
