9411ae2ba646cfa7c0065f6d4a8c1ee3b46e97be
ml242 <mattlucas@gmail.com>
2014-08-04 15:48:51 -0400

    style is working for the slider

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,36 +7,38 @@ $(function(){
     el.style.height = $(window).height() + 'px';
   });
 
-  var firstSectionHeight = $(window).height() - 128 + 'px';
+  var firstSectionHeight = $(window).height() - 101 + 'px';
 
   $('.main-intro').css({height: firstSectionHeight}); 
 
   $('.large-image-1').css({backgroundImage: 'url(http://media.giphy.com/media/QnWWQ1tVagHra/giphy.gif)'});
 
 
+  $('.carousel').carousel();
+
 
 // slider functionality
 
-  var slider = $('.testimonials-1');
+  // var slider = $('.testimonials-1');
 
-  $('.right-button').click(function(){
-    if ( slider.hasClass('testimonials-4') ) { 
-      slider = $('.testimonials-1');
-      slider.show().siblings().hide();
-    } else {
-      slider = slider.next();
-      slider.show().siblings().hide();
-      }
-    });
+  // $('.right-button').click(function(){
+  //   if ( slider.hasClass('testimonials-4') ) { 
+  //     slider = $('.testimonials-1');
+  //     slider.show().siblings().hide();
+  //   } else {
+  //     slider = slider.next();
+  //     slider.show().siblings().hide();
+  //     }
+  //   });
 
-  $('.left-button').click(function(){
-    if ( slider.hasClass('testimonials-1') ) { 
-      slider = $('.testimonials-4');
-      slider.show().siblings().hide();
-    } else {
-      slider = slider.prev();
-      slider.show().siblings().hide();
-      }
-    });
+  // $('.left-button').click(function(){
+  //   if ( slider.hasClass('testimonials-1') ) { 
+  //     slider = $('.testimonials-4');
+  //     slider.show().siblings().hide();
+  //   } else {
+  //     slider = slider.prev();
+  //     slider.show().siblings().hide();
+  //     }
+  //   });
 
 });
